ea1686c3bcedb10296fab8646eb5d38cb53233a3
nychavs <chavesnaiury@gmail.com>
2023-05-18 11:21:41 -0300

    maps backend done

--- web/src/pages/planta.jsx
+++ web/src/pages/planta.jsx
@@ -2,17 +2,37 @@ import Head from 'next/head'
 import React, { useEffect, useState } from "react";
 import Acessibilidade from "../components/Acessibilidade";
 import {Model} from "../components/mapa/Model"
+import { Avilla_Thinking } from '../components/Avilla_Thinking';
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import {OrbitControls, useGLTF} from '@react-three/drei'; 
-
-
+import { FaMapMarkedAlt } from 'react-icons/fa';
 // position={[-11,-3,-11]}
 
+
+const areas = [
+  {name:'Planta', model:'../components/mapa/Model'},
+  {name:'BTC', model:'../components/Avilla_Thinking'},
+  {name:'Chapeuzinho', model:''},
+  {name:'Eletrical Drives', model:''},
+  {name:'Power Tools', model:''},
+  {name:'Ambulatório', model:''},
+  {name:'Portarias', model:''}, 
+  {name:'Bosch Digital', model:''},
+  {name:'Digihub', model:''}
+]
+
+
 function Planta() {
+  const [model, setModel] = useState("Planta")
+  
+  function chooseMap(nome){
+    setModel(nome)  
+  }
+  
   return (
     <div className=' flex items-center justify-center'>
-    <div className="w-10/12 h-80">
+    <div className="flex w-9/12 h-[600px] flex-row">
           <Canvas>
             <Suspense fallback={null}>
               <ambientLight/>
@@ -22,7 +42,12 @@ function Planta() {
               penumbra={1}
               position={[10,15,10]}
               castShadow/>
-              <Model />
+              {model == "Planta" ? 
+              <Model /> 
+              : model == "BTC" ?
+              <Avilla_Thinking/> 
+              : null 
+            }
               <OrbitControls 
               enablePan={true}
               enableZoom={true}
@@ -31,17 +56,25 @@ function Planta() {
               />
             </Suspense>
           </Canvas>    
+          </div>
+          <div className='flex w-36 h-4/5 mx-8 items-center'>
+            {areas.length ? (
+              <ul>
+              {areas.map((areas)=>(
+                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 mb-2 w-52 rounded inline-flex items-center"
+                  onClick={() => chooseMap(areas.name)}
+                >
+                <div className='flex flex-row items-center'>
+                  <FaMapMarkedAlt/>
+                  <span className='px-3'>{areas.name}</span>
+                </div>
+              </button>
+              ))}
+              </ul>
+            ):null}
         </div>
             <div className="absolute bottom-2 w-full flex px-10 justify-evenly">
-                    <div className=''>
-                      <h1 className='text-sky-500	text-lg'>Origem</h1>
-                      <p>Informações sobre</p>
-                    </div>
                     <Acessibilidade />
-                    <div className=''>
-                      <h1 className='text-red-500	text-lg'>Destino</h1>
-                      <p>Informações sobre</p>
-                    </div>
             </div>
         </div>
   
