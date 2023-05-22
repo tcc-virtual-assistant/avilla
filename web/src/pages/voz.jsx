1abc36369bc10fec941fc0d82cc285e0ac3ea70b
João Vitor Montanari <joao.unipbr@gmail.com>
2023-05-21 23:40:04 -0300

    add reconhecimento de fala

--- web/src/pages/voz.jsx
+++ web/src/pages/voz.jsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
-import { FaMicrophoneAlt } from 'react-icons/fa'
 import React, { useState } from 'react';
+
+import { FaMicrophoneAlt } from 'react-icons/fa'
+import { MdRecordVoiceOver } from 'react-icons/md'
 
 export default function Voz() {
   const [speech, setSpeech] = useState('Olá, eu sou a Avila! Sua assistente virtual');
+  const [recognition, SetRecognition] = useState()
 
   const TextSpeech = () => {
     const mensagem = new SpeechSynthesisUtterance(speech);
@@ -16,6 +19,19 @@ export default function Voz() {
     window.speechSynthesis.speak(mensagem);
   }
 
+  const SpeakRecognition = () => {
+    const recognition = new window.webkitSpeechRecognition(); 
+    recognition.lang = 'pt-BR'; 
+
+    recognition.onresult = event => {
+      const resultado = event.results[0][0].transcript; 
+      SetRecognition(resultado);
+      console.log(recognition);
+    };
+
+    recognition.start(); 
+  };
+
   return (
     <>
       <Head>
@@ -25,13 +41,23 @@ export default function Voz() {
         <h1 className='my-24 text-center font-semibold text-5xl text-gray-400'>
           Avila Voz
         </h1>
-        <div className='w-full flex items-center justify-center'>
+        <div className='w-[80%] flex items-center m-auto justify-around'>
           <button 
             onClick={TextSpeech}
-            className='border-2 border-green-400 text-green-400 font-bold text-lg w-80 h-80 cursor-pointer rounded-full hover:bg-green-100 duration-300'
+            className='border-2 border-green-400 text-green-400 font-bold text-lg w-64 h-64 cursor-pointer rounded-full hover:bg-green-100 duration-300'
+          >
+            <MdRecordVoiceOver
+              size={100}
+              className='m-auto'
+            />
+          </button>
+
+          <button 
+            onClick={SpeakRecognition}
+            className='border-2 border-blue-400 text-blue-400 font-bold text-lg w-64 h-64 cursor-pointer rounded-full hover:bg-blue-100 duration-300'
           >
             <FaMicrophoneAlt
-              size={180}
+              size={100}
               className='m-auto'
             />
           </button>
