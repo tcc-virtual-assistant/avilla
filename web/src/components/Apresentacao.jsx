fa22e85f6389cf7f4888b487a587a6ee3e361501
Giovanna Freitas <giovanna5fsantos@gmail.com>
2023-05-24 15:53:05 -0300

    font-size repair

--- web/src/components/Apresentacao.jsx
+++ web/src/components/Apresentacao.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Apresentacao() {
   return (
     <>
-      <div className="mx-4 pt-8 flex flex-col text-center justify-center">
-        <div className="text-xl md:text-2xl pb-3 text-verde-escuro">
+      <div className="mx-4 pt-36 flex flex-col text-center justify-center">
+        <div className="text-xl md:text-3xl pb-3 text-verde-escuro">
           Olá! Sou Ávila, como posso te ajudar?
         </div>
-        <div className="text-xl md:text-2xl  text-verde-escuro">
+        <div className="text-xl md:text-3xl  text-verde-escuro">
           Caso não tenha ideia do que perguntar, aqui estão minhas sugestões!
         </div>
       </div>
